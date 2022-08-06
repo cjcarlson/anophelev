da248d63656ca78096818095f850ddccf39a8f2b
Colin J. Carlson <cjcarlson@berkeley.edu>
2022-08-06 13:40:32 -0400

    Completely redone!

---
+++ Figure 1.R
@@ -0,0 +1,81 @@
+
+library(tidyverse); library(magrittr); library(MetBrewer)
+
+anoph <- read_csv('C:/Users/cjcar/Dropbox/AnophElev/clean/Anopheles clean.csv')
+anoph %<>% select(-X, -X1)
+
+anoph %>%
+  select(Lat, Long, Year) %>%
+  distinct() %>%
+  group_by(Lat, Long) %>%
+  summarize(Years = diff(range(Year))+1) -> coverage
+
+coverage %<>% arrange(Years)
+
+world <- map_data("world")
+africa <- subset(world, region %in% c("Algeria","Angola","Benin","Botswana","Burkina Faso","Burundi",
+                                      "Cabo Verde","Cameroon","Central African Republic","Chad","Comoros",
+                                      "Democratic Republic of the Congo","Republic of Congo","Ivory Coast",
+                                      "Djibouti","Egypt","Equatorial Guinea","Eritrea","Swaziland","Ethiopia",
+                                      "Gabon","Gambia","Ghana","Guinea","Guinea-Bissau","Kenya","Lesotho","Liberia",
+                                      "Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco",
+                                      "Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe",
+                                      "Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan",
+                                      "Sudan","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"))
+# Borrowed from https://warin.ca/posts/rcourse-datavisualizationwithr-maps/
+coverage %>%
+  ggplot(aes(x = Long, y = Lat, color = Years)) + 
+  geom_point(size = 2) + 
+  scale_color_gradientn(colors = met.brewer("Hokusai3")) + 
+  geom_polygon(data = africa, aes(x = long, y = lat, group = group), 
+               fill = NA, color = "grey50", alpha = 0.3) + 
+  theme_void() + theme(legend.position = 'right',
+                       plot.margin = margin(1,1,0,0, "cm"))
+
+########### ELEVATION AND REGIONS
+
+library(raster)
+library(sf)
+
+elev <- raster("C:/Users/cjcar/Dropbox/AnophElev/Data/Africa.asc")
+elev %<>% aggregate(fact = 10, fun = mean)
+
+gbd <- st_read(dsn = "C:/Users/cjcar/Dropbox/continents/GBD-Retool-DECIMALS/OriginalGBD",
+               layer = "WorldRegions")
+
+gbd %<>% filter(SmllRgn %in% c("Sub-Saharan Africa (East)",
+                               "Sub-Saharan Africa (West)",
+                               "Sub-Saharan Africa (Central)",
+                               "Sub-Saharan Africa (Southern)"))
+# gbd %<>% select()
+
+gbd %<>% group_by(SmllRgn) %>% summarize()
+
+elev %<>% as.data.frame(xy = TRUE) %>%
+  drop_na() %>% rename(Elevation = africa)
+
+ggplot(elev) + 
+  geom_raster(aes(x = x, y = y, fill = Elevation)) + 
+  scale_fill_gradientn(colors = met.brewer("Tam"), name = 'Elevation (m)') + 
+  theme_void() +   
+  xlim(range(africa$long)) + ylim(-35, 38) + 
+  geom_polygon(data = africa, aes(x = long, y = lat, group = group), 
+               fill = NA, color = "grey50", alpha = 0.3) + 
+  geom_sf(data = gbd, fill = NA, color = NA, lwd = 1) + # turn back on for outlines
+  theme(legend.position = 'right') -> g1
+
+ggplot(coverage) + 
+  geom_polygon(data = africa, aes(x = long, y = lat, group = group), 
+               fill = NA, color = "grey50", alpha = 0.3) + 
+  theme_void() + 
+  geom_point(aes(x = Long, y = Lat, color = Years), size = 2) + 
+  scale_color_gradientn(colors = met.brewer("Hokusai3")) + 
+  # theme(legend.position = 'right',
+  #       plot.margin = margin(1,1,0,0, "cm")) + 
+  xlim(range(africa$long)) + ylim(-35, 38) + 
+  geom_sf(data = gbd, fill = NA, color = NA, lwd = 1) + 
+  theme(legend.position = 'right') -> g2
+
+library(patchwork)
+g1 + g2 + 
+  plot_layout(guides = 'collect') + plot_annotation(tag_levels = c("A","B"))
---
+++ Figure 2 and estimates.R
@@ -0,0 +1,208 @@
+
+library(tidyverse); library(magrittr); library(patchwork); library(MetBrewer)
+
+anoph <- read_csv('C:/Users/cjcar/Dropbox/AnophElev/clean/Anopheles clean.csv')
+anoph %<>% dplyr::select(-X, -X1)
+
+anoph %<>% mutate(Species = str_replace(Species,"\\..*","")) %>%
+  filter(!is.na(elev)) 
+
+splist <- anoph %>% pull(Species) %>% unique() %>% sort()
+
+# Generate a list of models
+
+mods <- lapply(splist, function(sp) {  
+  
+  spdf <- anoph %>% filter(Species == sp)
+  latdf <- spdf %>% 
+    filter(Lat < 0) %>% # Only southern hemisphere
+    group_by(Year) %>% summarize(Lat = min(Lat)) %>% mutate(Lat = -1*Lat)
+  elevdf <- spdf %>% group_by(Year) %>% summarize(elev = max(elev))
+  spyrs <- range(spdf$Year)
+  
+  if(length(unique(latdf$Lat)) > 4) {
+    latlm <- lm(Lat ~ Year, data = latdf)
+    predlat <- data.frame(species = sp,
+                          latslope = coef(latlm)[[2]],
+                          latsig = coefficients(summary(latlm))[2,4])
+  } else {
+    predlat <- data.frame(species = sp,
+                          latslope = NA,
+                          latsig = NA)
+  }
+  
+  if(length(unique(elevdf$elev)) > 4) {
+    elevlm <- lm(elev ~ Year, data = elevdf)
+    predelev <- data.frame(species = sp,
+                          elevslope = coef(elevlm)[[2]],
+                          elevsig = coefficients(summary(elevlm))[2,4])
+  } else {
+    predelev <- data.frame(species = sp,
+                           elevslope = NA,
+                           elevsig = NA)
+  }
+  
+  sppred <- left_join(predlat,predelev)
+  sppred$Species <- sp
+  return(sppred)
+  
+})
+
+mods <- bind_rows(mods)
+
+# Some of the simpler analyses in the text
+
+mean(mods$latslope, na.rm = TRUE)*111
+table(mods$latsig < 0.05)
+
+mean(mods$elevslope, na.rm = TRUE)
+table(mods$elevsig < 0.05)
+
+cor.test(mods$latslope, mods$elevslope, method = 'pearson')
+
+# Generate the figures
+
+spdfs <- lapply(splist, function(sp) {  
+  
+  spdf <- anoph %>% filter(Species == sp)
+  latdf <- spdf %>% 
+    filter(Lat < 0) %>% 
+    group_by(Year) %>% summarize(Lat = min(Lat)) %>% mutate(Lat = -1*Lat)
+  elevdf <- spdf %>% group_by(Year) %>% summarize(elev = max(elev))
+  spyrs <- range(spdf$Year)
+  
+  if(length(unique(latdf$Lat)) > 4) {
+    latlm <- lm(Lat ~ Year, data = latdf)
+    predlat <- data.frame(Year = c(1898:2016),
+                        Lat = predict(latlm, data.frame(Year = c(1898:2016))),
+                        LatSig = (summary(latlm)$coefficients[2,4] < 0.05/22))
+  } else {
+    predlat <- data.frame(Year = c(1898:2016),
+                          Lat = NA,
+                          LatSig = NA)
+  }
+  
+  if(length(unique(elevdf$elev)) > 4) {
+    elevlm <- lm(elev ~ Year, data = elevdf)
+    predelev <- data.frame(Year = c(1898:2016),
+                          Elev = predict(elevlm, data.frame(Year = c(1898:2016))),
+                          ElevSig = (summary(elevlm)$coefficients[2,4] < 0.05/22))
+  } else {
+    predelev <- data.frame(Year = c(1898:2016),
+                          Elev = NA,
+                          ElevSig = NA)
+  }
+  
+  sppred <- left_join(predlat,predelev)
+  sppred$Species <- sp
+  return(sppred)
+  
+})
+
+models <- bind_rows(spdfs) %>% as_tibble()
+models %<>% mutate(LatSig = factor(LatSig, levels = c("TRUE", "FALSE")))
+models %<>% mutate(ElevSig = factor(ElevSig, levels = c("TRUE", "FALSE")))
+
+ggplot(models, aes(x = Year, y = Lat, group = Species)) + 
+  geom_line(aes(color = Species, linetype = LatSig), size = 1) + 
+  theme_bw() + 
+  guides(linetype = "none") +  
+  ylab("Latitude (°S)") +
+  scale_color_manual(values = met.brewer("Renoir", n = 22)) + 
+  xlab("") -> lat
+
+ggplot(models, aes(x = Year, y = Elev, group = Species)) + 
+  geom_line(aes(color = Species, linetype = ElevSig), size = 1) + 
+  theme_bw() + 
+  guides(linetype = "none") + 
+  ylab("Elevation (m)") +
+  scale_color_manual(values = met.brewer("Renoir", n = 22)) + 
+  xlab("") -> elev
+
+models
+
+lat / elev 
+
+#
+#
+#
+#
+
+models %>%
+  group_by(Species) %>%
+  filter(Year==1900) %>% 
+  dplyr::select(Species, Lat) %>%
+  rename(Lat1900 = Lat) -> df1900
+
+models %>%
+  group_by(Species) %>%
+  filter(Year==2000) %>%
+  dplyr::select(Species, Lat) %>%
+  rename(Lat2000 = Lat) -> df2000
+
+models %>%
+  dplyr::select(Species, LatSig) %>%
+  distinct() -> latsigs
+
+anoph %>%
+  filter(Lat < 0, Year %in% c(1900:1950)) %>%
+  group_by(Species) %>%
+  slice(which.min(Lat)) %>%
+  left_join(df1900) %>% 
+  left_join(df2000) %>% 
+  left_join(latsigs) -> mappy
+
+
+
+world <- sf::st_as_sf(maps::map("world", plot = FALSE, fill = TRUE))
+
+ggplot(mappy) + 
+  geom_sf(data = world, fill = NA, color = "black", alpha = 0.3, size = 0.2) + 
+  geom_segment(aes(x = Long, y = -1*Lat1900, xend = Long, yend = -1*Lat2000, color = LatSig),
+               lineend = 'round', linejoin = 'round', 
+               size = 1,
+               arrow = arrow(length = unit(0.1, "inches")),
+               position = position_jitter(height = 0, width = 1.1)) + 
+  xlim(5, 40) + ylim(-30,5) + theme_bw() + xlab("") + ylab("") +
+  scale_color_manual(values = c('indianred2','grey30')) + 
+  guides(color = "none") -> g1
+ 
+
+models %>% 
+  filter(Year == 1900) %>%
+  dplyr::select(Species, Elev) %>%
+  rename(Elev1900 = Elev) -> df1900
+
+models %>% 
+  filter(Year == 2000) %>%
+  dplyr::select(Species, Elev) %>%
+  rename(Elev2000 = Elev) -> df2000
+
+models %>%
+  dplyr::select(Species, ElevSig) %>%
+  distinct() -> elevsigs
+
+anoph %>%
+  filter(Year == 1900) %>% 
+  group_by(Species) %>%
+  slice(which.max(elev)) %>%
+  dplyr::select(Species, elev) %>%
+  left_join(df1900) %>% 
+  left_join(df2000) %>% 
+  left_join(elevsigs) -> mappy2
+
+
+tri <- data.frame(x = c(0,  3000, 3000),
+                  y = c(0, 0,     3000))
+ggplot(mappy2) +
+  geom_polygon(data = tri, aes(x = x, y = y), alpha = .75, fill = 'gray90', color = 'black') + 
+  geom_segment(aes(x = Elev1900, y = Elev1900, xend = Elev1900, yend = Elev2000, color = ElevSig),
+               lineend = 'round', linejoin = 'round', 
+               size = 0.8,
+               arrow = arrow(length = unit(0.1, "inches")),
+               position = position_jitter(height = 0, width = 1.1)) + 
+  xlab("") + ylab("") +
+  scale_color_manual(values = c('indianred2','grey30'))  + 
+  guides(color = "none") + theme_bw() -> g2
+
+g1 + g2 + plot_annotation(tag_levels = c("A","B"))
